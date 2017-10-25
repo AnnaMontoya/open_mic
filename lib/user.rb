f2d3661f76dd6fd266ee46466eb11fd329079097
AnnaMontoya <anna.l.montoya@gmail.com>
2017-10-25 10:53:31 -0600

    interation 3

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,20 @@
+require './lib/joke'
+
 class User
-  attr_reader :name
+  attr_reader :name, :jokes
 
   def initialize(name)
-    @name = name
+    @name   = name
+    @jokes  = []
+    @told   = 0
   end
+
+  def learn(joke)
+    @jokes << joke
+  end
+
+  def tell(name, joke)
+  
+  end
+
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -21,4 +21,34 @@ class UserTest < Minitest::Test
 
     assert_equal "Ali", ali.name
   end
+
+  def test_it_has_jokes
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_instance_of Joke, joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+  end
+
+  def test_user_has_a_collection_of_jokes
+    sal = User.new("Sal")
+
+    assert_equal [], sal.jokes
+  end
+
+  def test_user_can_learn_jokes
+    sal = User.new("Sal")
+
+    sal.learn({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    sal.jokes
+  end
+
+  def test_users_can_tell_jokes
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+
+    sal.tell(User.new("Ali"), Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."}))
+    ali.learn(joke)
+    ali.jokes
+  end
+
 end
